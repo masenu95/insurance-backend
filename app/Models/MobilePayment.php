bae7583561ecb833ee486d1c717a1ec1ef9bb0e4
masenu95 <masenu95@gmail.com>
2023-07-08 23:28:14 +0300

    masenu

--- app/Models/MobilePayment.php
+++ app/Models/MobilePayment.php
@@ -9,6 +9,8 @@ class MobilePayment extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['order_id','currency','amount','paid_amount','remain_amount','payment_status','payment_token','qr','channel','phone','transid','reference','status','is_deleted'];
+
     protected $hidden = ["created_at", "updated_at"];
-    
+
 }
